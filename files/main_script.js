e1cc62116583647ce28f2f20d8be0bfff3e49cd5
dltjdud808 <53907861+dltjdud808@users.noreply.github.com>
2020-12-15 20:46:03 +0900

    데이터 시각화
    메인에 그래프 샘플 넣음

--- files/main_script.js
+++ files/main_script.js
@@ -36,46 +36,52 @@ function carousel(){
 }
 
 // 메인 페이지 영화 목록 2 Score 육각형 그래프
-var ctx = document.getElementById("myChart").getContext('2d');
+
+var ctx = document.getElementById("myChart");
 /*
 - Chart를 생성하면서, 
 - ctx를 첫번째 argument로 넘겨주고, 
 - 두번째 argument로 그림을 그릴때 필요한 요소들을 모두 넘겨줍니다. 
 */
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+  type: "radar",
+  data: {
+    labels: ["S", "M", "V", "A", "G", "E"],
+    datasets: [
+          {
+              label: "User",
+              data: [8, 6, 10, 2, 7, 5],
+              backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)"
+              ],
+              borderColor: [
+                  "rgba(255, 99, 132, 1)"
+              ],
+              borderWidth: 1,
+          },
+          {
+              label: "Critic",
+              data: [6, 7, 8, 3, 9, 4],
+              backgroundColor: [
+                  "rgba(75, 192, 192, 0.2)"
+              ],
+              borderColor: [
+                  "rgba(75, 192, 192, 1)"
+              ],
+              borderWidth: 1,
+          },
+    ],
+  },
+  options: {
+    scale: {
+      angleLines: {
+        display: false,
+      },
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 10,
+      },
     },
-    options: {
-        maintainAspectRatio: true, // default value. false일 경우 포함된 div의 크기에 맞춰서 그려짐.
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
+  },
 });
