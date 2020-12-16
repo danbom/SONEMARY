e9b45cd60d4f0da381059bfffb251084b8f98b74
Ming <71026706+mina1040@users.noreply.github.com>
2020-12-17 00:10:04 +0900

    php

--- files/login_ok.php
+++ files/login_ok.php
@@ -3,21 +3,21 @@
 	header('Content-Type: text/html; charset=utf-8');
     error_reporting(1);
     ini_set("display_errors",1);
-    
-    
+
+
     $host = 'localhost';
     $user = 'root';
-    $pw = 'min01038082726!';
+    $pw = 'ming0';
     $dbName = 'sonemary';
-    
+
     $connect =mysqli_connect($host, $user, $pw, $dbName);
-    
+
     if(mysqli_connect_error()){
       echo "mysql 접속 중 오류 발생";
       echo mysqli_connect_error();
     }
-    
-    mysqli_query($conn,'SET NAMES utf8');
+
+    mysqli_query($connect,'SET NAMES utf8');
 
 
 	session_start(); // 세션
@@ -42,6 +42,6 @@
 
 	}
 
-	
+
 
   ?>
