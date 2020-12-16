e309589e5c3b4299d1afc8bc7f633cb11835a91f
danbom <bear0369@naver.com>
2020-12-16 19:49:34 +0900

    top10페이지 쓸데없는 글자 삭제 signin.php 수정

--- files/signin.php
+++ files/signin.php
@@ -1,7 +1,25 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
+require_once("db.php");
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+error_reporting(1);
+ini_set("display_errors",1);
 
-include "lib.php";
+
+$host = 'localhost';
+$user = 'root';
+$pw = '1540';
+$dbName = 'sonemary';
+
+$connect =mysqli_connect($host, $user,$pw, $dbName);
+print_r($connect);
+
+if(mysqli_connect_error()){
+  echo "mysql 접속 중 오류 발생";
+  echo mysqli_connect_error();
+}
+
+mysqli_query($conn,'SET NAMES utf8');
 
 $user_id = $_POST['user_id'];
 $password=$_POST['password'];
@@ -12,6 +30,7 @@ $sql = "INSERT INTO user (user_id,password,critic) VALUES ('".$user_id."','".$pa
 
 
 ?>
+
 
 <meta charset="utf-8" />
 <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
