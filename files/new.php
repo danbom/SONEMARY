8c334612c092ec06b760f0bc4ff6641f1fe51775
Ming <71026706+mina1040@users.noreply.github.com>
2020-12-16 20:46:25 +0900

    new.php

---
+++ files/new.php
@@ -0,0 +1,221 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Sonemary</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+	<div id="app">
+
+		<header id="main-header">
+			<div id="logo">
+				<a href="main_page.html"><img src="image/SONEMARY.png"></a>
+			</div>
+			<div id="username">
+				<a href="my_page.html">Welcome, USER <img src="image/profile-user.png"></a>
+      </div>
+				<nav>
+					<ul id="menu-list">
+						<li><a href="genre.html" id="ami"> Genre </a></li>
+						<li><a href="top10.html" id="ami"> Top10 </a></li>
+						<li><a href="new.html" id="ami"> New </a></li>
+					</ul>
+				</nav>
+				<form id="search" onClick="location.href='search_page.php'">
+					<input type="text" id="searchKey" name="searchKey" placeholder="search...">
+	        <button type="submit" id="searchBtn"><img src="image/loupe.png"></button>
+				</form>
+		</header>
+
+		<article class="main-article" style="padding-bottom: 15%;">
+
+				<header class="section1-header">
+					<h2>New</h2>
+                </header>
+                <br>
+
+				<!-- 영화 목록 2 -->
+                <div class="section7">
+                    <div class="section7-contents">
+                      <div class="section7-img">
+                        <a href="detail_page.html">
+                          <img src="image/movie_spiderman_poster.jpg"/>
+                        </a>
+                      </div>
+
+                      <div class="section7-info">
+                        <div>
+                          <div class="title" style="font-size: 18px; font-weight: bold;">
+                            Spider-Man: Homecoming
+                          </div>
+                          <div class="info-details">
+                            <p>Action, Adventure, SF | USA | 2017.07.05 <br />
+                              2h 13m | R12</p>
+                          </div>
+                          <div class="info-badge">
+                            <img src="image/badge_acting.png" />
+                          </div>
+                        </div>
+                        <div class="info-review">
+                          <a href="review_page.html"> Write your Review </a>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                    <div class="section7-contents">
+
+                        <div class="section7-img">
+                          <a href="detail_page.html">
+                            <img src="image/movie_spiderman_poster.jpg"/>
+                          </a>
+                        </div>
+
+                        <div class="section7-info">
+                          <div>
+                            <div class="title" style="font-size: 18px; font-weight: bold;">
+                              Spider-Man: Homecoming
+                            </div>
+                            <div class="info-details">
+                              <p>Action, Adventure, SF | USA | 2017.07.05 <br />
+                                2h 13m | R12</p>
+                            </div>
+                            <div class="info-badge">
+                              <img src="image/badge_acting.png" />
+                            </div>
+                          </div>
+                          <div class="info-review">
+                            <a href="review_page.html"> Write your Review </a>
+                          </div>
+                        </div>
+
+
+                      </div>
+
+
+                    <div class="section7-contents">
+                      <div class="section7-img">
+                        <a href="detail_page.html">
+                          <img src="image/movie_spiderman_poster.jpg"/>
+                        </a>
+                      </div>
+
+                      <div class="section7-info">
+                        <div>
+                          <div class="title" style="font-size: 18px; font-weight: bold;">
+                            Spider-Man: Homecoming
+                          </div>
+                          <div class="info-details">
+                            <p>Action, Adventure, SF | USA | 2017.07.05 <br />
+                              2h 13m | R12</p>
+                          </div>
+                          <div class="info-badge">
+                            <img src="image/badge_acting.png" />
+                          </div>
+                        </div>
+                        <div class="info-review">
+                          <a href="review_page.html"> Write your Review </a>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                    <div class="section7-contents">
+                      <div class="section7-img">
+                        <a href="detail_page.html">
+                          <img src="image/movie_spiderman_poster.jpg"/>
+                        </a>
+                      </div>
+
+                      <div class="section7-info">
+                        <div>
+                          <div class="title" style="font-size: 18px; font-weight: bold;">
+                            Spider-Man: Homecoming
+                          </div>
+                          <div class="info-details">
+                            <p>Action, Adventure, SF | USA | 2017.07.05 <br />
+                              2h 13m | R12</p>
+                          </div>
+                          <div class="info-badge">
+                            <img src="image/badge_acting.png" />
+                          </div>
+                        </div>
+                        <div class="info-review">
+                          <a href="review_page.html"> Write your Review </a>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                    <div class="section7-contents">
+                      <div class="section7-img">
+                        <a href="detail_page.html">
+                          <img src="image/movie_spiderman_poster.jpg"/>
+                        </a>
+                      </div>
+
+                      <div class="section7-info">
+                        <div>
+                          <div class="title" style="font-size: 18px; font-weight: bold;">
+                            Spider-Man: Homecoming
+                          </div>
+                          <div class="info-details">
+                            <p>Action, Adventure, SF | USA | 2017.07.05 <br />
+                              2h 13m | R12</p>
+                          </div>
+                          <div class="info-badge">
+                            <img src="image/badge_acting.png" />
+                          </div>
+                        </div>
+                        <div class="info-review">
+                          <a href="review_page.html"> Write your Review </a>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                  </div>
+
+
+			</article>
+    </article>
+
+    <br>
+    <div class="footer">
+      <div class="footer_box" style="display: flex; background-color: black; color: white; width: 100%; text-align: left; vertical-align: middle; padding-top:5%; padding-bottom:5%; padding-left:5%">
+        <div class="left_box" style="float:left; width:50%; background-color: black; color: white;">
+          <h3>SONEMARY<br></h3>
+          <h4>2020 Open Software Platform Team8</h4>
+        </div>
+        <div class="center_box" style="float:left; width:50%; background-color: black; color: white;">
+          SITE<br><br>
+          <h4 id="site-define">Main<br></h4>
+          <h4 id="site-define">Genre<br></h4>
+          <h4 id="site-define">Top10<br></h4>
+          <h4 id="site-define">New<br></h4>
+          <h4 id="site-define">My<br></h4>
+        </div>
+        <div class="right_box" style="float:left; width:50%; background-color: black; color: white;">
+      CONTECT<br><br>
+      DEVELPED BY<br><br>
+      <h4 id="site-define">
+        Eunyoung (danbom)<br>
+        Sunwoo (mallang7)<br>
+        MinA (Ming)<br>
+        Seoyoung (dltjdud808)
+      </h4>
+      </div>
+      </div>
+  </div>
+	</div>
+</body>
+</html>
